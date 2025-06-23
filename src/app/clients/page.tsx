2e5a4cf0101e39863734721fd4c7008202e74a92
Temitope Ogunrekun <olalekanogunrekun@gmail.com>
2025-06-23 19:27:21 +0100

    fixed an indention error

--- src/app/clients/page.tsx
+++ src/app/clients/page.tsx
@@ -1,16 +1,17 @@
-import ClientForm from '@/components/ClientForm'
-import React from 'react'
+import ClientForm from "@/components/ClientForm";
+import React from "react";
 
 const ClientsPage = () => {
-    return (
-        <main className='p-6'>
-            <h1 className='text-3xl font-bold mb-4'>Clients</h1>
-            <p className='text-gray-600'>This is where your list of clients will show</p>
+	return (
+		<main className="p-6">
+			<h1 className="text-3xl font-bold mb-4">Clients</h1>
+			<p className="text-gray-600 mb-5">
+				This is where your list of clients will show
+			</p>
 
-            <ClientForm />
-      </main>
+			<ClientForm />
+		</main>
+	);
+};
 
-  )
-}
-
-export default ClientsPage
+export default ClientsPage;
